751b2939177cdf0699c612fae5080663ac9526da
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-06 10:08:38 -0300

    chore(validator): uso de code formatter na classe DataValidator

--- src/main/java/com/poliedro/jogodonotao/utils/DataValidator.java
+++ src/main/java/com/poliedro/jogodonotao/utils/DataValidator.java
@@ -3,32 +3,45 @@ package com.poliedro.jogodonotao.utils;
 /**
  * Utilitário para validação de dados.
  * Contém métodos estáticos para validar dados, como nome, e-mail acadêmico e RA (registro de matrícula), conforme o formato do Colégio Poliedro.
+ *
  * @author Alexandre Raminelli
  */
 public class DataValidator {
     // regex (expressão regular): retorna true se a string seguir o formato especificado.
-    /** Regex para validar nomes. */
+    /**
+     * Regex para validar nomes.
+     */
     private final static String REGEX_NAME = "^([\\p{L}]+(?:\\h+[\\p{L}]+)*)$";
 
-    /** Regex para validar RA (registro de matrícula) do Colégio Poliedro. */
+    /**
+     * Regex para validar RA (registro de matrícula) do Colégio Poliedro.
+     */
     private final static String REGEX_RA = "^\\d{2}\\.\\d{3}-\\d$";
 
-    /** Regex para validar e-mail de alunos (@p4ed.com). */
+    /**
+     * Regex para validar e-mail de alunos (@p4ed.com).
+     */
     private final static String REGEX_EMAIL_ALUNO = "^[a-zA-Z0-9._%+-]+@p4ed\\.com$";
 
-    /** Regex para validar e-mail de professores (@sistemapoliedro.com.br). */
+    /**
+     * Regex para validar e-mail de professores (@sistemapoliedro.com.br).
+     */
     private final static String REGEX_EMAIL_PROFESSOR = "^[a-zA-Z0-9._%+-]+@sistemapoliedro\\.com\\.br$";
 
 
-    /** Método para validar um nome. O nome é válido se ele possui somente caracteres alfabéticos, sem númerais ou caracteres especiais.
+    /**
+     * Método para validar um nome. O nome é válido se ele possui somente caracteres alfabéticos, sem númerais ou caracteres especiais.
+     *
      * @param nome Nome a ser validado.
      * @return Se o nome é valido, retorna {@code true}. Caso contrário, retorna {@code false}.
      */
-    public static boolean isNomeValido(String nome){
+    public static boolean isNomeValido(String nome) {
         return nome.matches(REGEX_NAME);
     }
 
-    /** Método para validar o RA (registro de matrícula).
+    /**
+     * Método para validar o RA (registro de matrícula).
+     *
      * @param ra RA a ser validado.
      * @return Se o RA é válido e segue o padrão do Colégio Poliedro, retorna {@code true}. Caso contrário, retorna {@code false}.
      */
@@ -38,6 +51,7 @@ public class DataValidator {
 
     /**
      * Método para validar o e-mail acadêmico do aluno.
+     *
      * @param email E-mail a ser validado.
      * @return Se o e-mail informado seguir o formato do e-mail acadêmico de aluno (@p4ed.com), retorna {@code true}. Caso contrário, retorna {@code false}.
      */
@@ -47,6 +61,7 @@ public class DataValidator {
 
     /**
      * Método para validar o e-mail do professor.
+     *
      * @param email E-mail a ser validado.
      * @return Se o e-mail informado seguir o formato do e-mail do professor (@sistemapoliedro.com.br), retorna {@code true}. Caso contrário, retorna {@code false}.
      */
