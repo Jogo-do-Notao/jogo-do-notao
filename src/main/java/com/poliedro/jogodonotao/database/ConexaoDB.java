9bbfaa753caec894e86f83cef685afbf2c80c9f2
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-06 13:45:40 -0300

    Squashed merge feature/database/connection
    commit 003521728a76f9c6d684eeee952b8cc9a50541a7
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 6 13:45:01 2025 -0300
    
        feat(database-connection): criação de classe para a conexão com o banco de dados
    
    commit 267ab232b70dee5b57672c6a09aae091bb039d58
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 6 13:34:13 2025 -0300
    
        feat(dependencies): adição da dependência mysql-connector-j para conexão do banco de dados SQL
    
    commit 5d3880a41e60f6640d097f447430cff05639a343
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 6 11:37:46 2025 -0300
    
        feat(dependencies): adição da dependência io.github.cdimascio:dotenv-java para carregar variáveis de ambiente

--- src/main/java/com/poliedro/jogodonotao/database/ConexaoDB.java
+++ src/main/java/com/poliedro/jogodonotao/database/ConexaoDB.java
@@ -1,4 +1,66 @@
 package com.poliedro.jogodonotao.database;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Classe responsável pela conexão com o banco de dados MySQL no Aiven.
+ *
+ * @author Alexandre Raminelli
+ */
 public class ConexaoDB {
+    /**
+     * Conexão com o banco de dados MySQL no Aiven.
+     */
+    private static Connection connection;
+
+    /**
+     * Obtêm uma instância única da conexão com o banco de dados.
+     *
+     * @return Conexão com o banco de dados MySQL no Aiven.
+     */
+    public static Connection getConnection() throws SQLException {
+        // Verificar se há uma conexão ativa
+        if (connection == null || connection.isClosed()) {
+            /* Se não houver uma conexão ativa, cria uma nova conexão */
+            try {
+                // Carregar o driver do MySQL
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                // Carregar URI das variáveis de ambiente
+                Dotenv dotenv = Dotenv.load();
+                String uri = dotenv.get("DB_SERVICE_URI");
+
+                // Estabelecer conexão com o banco de dados usando o URI
+                connection = DriverManager.getConnection(uri);
+
+            } catch (SQLException | ClassNotFoundException e) {
+                // Se ocorrer um erro
+                System.err.println(
+                        "Erro ao conectar ao banco de dados: \n" +
+                                e.getMessage()
+                );
+            }
+        }
+        // Retornar a conexão
+        return connection;
+    }
+
+    /**
+     * Verifica se a conexão com o banco de dados está ativa.
+     *
+     * @return {@code true} se a conexão estiver ativa e funcionando, ou {@code false} caso contrário.
+     */
+    public static boolean checkConnection() {
+        try {
+            Connection conexao = getConnection(); // obter a conexão
+            // retorna true se a conexão estiver ativa e aberta
+            return conexao != null && !conexao.isClosed();
+        } catch (SQLException e) {
+            return false; // se ocorrer uma exceção, retorna false
+        }
+    }
 }
