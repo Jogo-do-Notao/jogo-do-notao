74f18d94ddcb70414303d1bc273f13abaa1257f9
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-08 11:54:51 -0300

    feat(tela-login): adição de teste de conexão a Internet e banco de dados

--- src/main/java/com/poliedro/jogodonotao/controller/ControleLogin.java
+++ src/main/java/com/poliedro/jogodonotao/controller/ControleLogin.java
@@ -1,5 +1,7 @@
 package com.poliedro.jogodonotao.controller;
 
+import com.poliedro.jogodonotao.database.ConexaoDB;
+import com.poliedro.jogodonotao.utils.ConexaoInternet;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -7,6 +9,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
+
+import javax.swing.JOptionPane;
 
 /**
  * Classe controladora da tela de login.
@@ -56,13 +60,44 @@ public class ControleLogin {
     /** Método que autentica o aluno. */
     @FXML
     void autenticarAluno(ActionEvent event) {
+        // Verificar conexão com Internet e banco de dados
+        checkConexao();
 
     }
 
     /** Método que autentica o professor. */
     @FXML
     void autenticarProfessor(ActionEvent event) {
-
+        // Verificar conexão com Internet e banco de dados
+        checkConexao();
     }
 
+    /**
+     * Método para verificar a conexão com a Internet e banco de dados.
+     * Se houver um problema com a conexão, mostra um aviso ao usuário.
+     */
+    void checkConexao() {
+        // Verificar conexão com a Internet
+        if (!ConexaoInternet.checkInternet()) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Não foi possível acessar o sistema porque seu dispositivo está sem conexão com a Internet. \nVerifique se o Wi-Fi ou os dados móveis estão ativados e funcionando corretamente. \nO login só pode ser realizado quando houver conexão ativa.",
+                    "Sem conexão com a Internet",
+                    JOptionPane.WARNING_MESSAGE // ícone de aviso
+            );
+            return; // sair do método
+        }
+        System.out.println("Nenhum problema com a conexão a Internet.");
+        // Verificar conexão com o banco de dados
+        if (!ConexaoDB.checkConnection()) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Estamos enfrentando dificuldades para acessar os dados necessários. \nIsso pode ter ocorrido por instabilidade no servidor ou manutenção temporária. \nPor favor, tente novamente em alguns minutos. \nSe o problema continuar, entre em contato com o suporte da escola.",
+                    "Erro ao conectar ao sistema",
+                    JOptionPane.WARNING_MESSAGE // ícone de aviso
+            );
+            return; // sair do método
+        }
+        System.out.println("Nenhum problema com a conexão com o banco de dados.");
+    }
 }
