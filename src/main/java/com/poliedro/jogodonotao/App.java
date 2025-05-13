19c1270f270613702a3385030b3e1fbc35cb3f33
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-13 12:23:28 -0300

    feat(font): aplicação da fonte Ubuntu

--- src/main/java/com/poliedro/jogodonotao/App.java
+++ src/main/java/com/poliedro/jogodonotao/App.java
@@ -3,6 +3,7 @@ package com.poliedro.jogodonotao;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,16 +11,27 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Carregar fonte personalizada
+        Font.loadFont(getClass().getResourceAsStream("fonts/Ubuntu-Bold.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Ubuntu-BoldItalic.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Ubuntu-Italic.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Ubuntu-Light.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Ubuntu-LightItalic.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Ubuntu-Medium.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Ubuntu-MediumItalic.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Ubuntu-Regular.ttf"), 12);
+
         // Carregar arquivo FXML (Scene Builder) como scene
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("views/tela-login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         // Título da janela
         stage.setTitle("Jogo do Notão");
-
         // Definir tamanho mínimo da janela
         stage.setMinWidth(800);
         stage.setMinHeight(600);
+        // Fonte padrão
+        scene.getRoot().setStyle("-fx-font-family: 'Ubuntu';");
 
         stage.setScene(scene); // Atribuir scene ao stage
         stage.show(); // exigir stage
