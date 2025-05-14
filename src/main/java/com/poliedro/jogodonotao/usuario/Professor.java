3742f663a3cb303524e072c52356e0bc058c673b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-14 14:50:35 -0300

    feat(professor): criação dos atributos e getters da classe `Professor`

--- src/main/java/com/poliedro/jogodonotao/usuario/Professor.java
+++ src/main/java/com/poliedro/jogodonotao/usuario/Professor.java
@@ -3,5 +3,44 @@ package com.poliedro.jogodonotao.usuario;
 /**
  * Classe que representa um professor do jogo.
  */
-public class Professor extends Usuario{
+public class Professor extends Usuario {
+    /**
+     * Descrição do professor, como matérias e séries lecionadas.
+     */
+    private String descricao;
+    /**
+     * Se o professor possui permissão para cadastrar outros professores.
+     */
+    private boolean coordenador = false;
+
+    // Construtores
+
+    /**
+     * Construtor a partir do ID do banco de dados.
+     */
+    public Professor(int id) {
+        //
+    }
+
+    /**
+     * Construtor a partir das informações.
+     */
+    public Professor(int id, String nome, String email, String senha, String descricao, boolean coordenador) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.descricao = descricao;
+        this.coordenador = coordenador;
+    }
+
+    // Getters
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isCoordenador() {
+        return coordenador;
+    }
 }
