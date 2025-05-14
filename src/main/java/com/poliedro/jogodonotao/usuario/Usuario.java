e7686ba14d506794d44495ae4fe8bb43c964726e
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-14 14:34:21 -0300

    feat(class): Atributos e métodos da classe `Usuario`

--- src/main/java/com/poliedro/jogodonotao/usuario/Usuario.java
+++ src/main/java/com/poliedro/jogodonotao/usuario/Usuario.java
@@ -5,4 +5,37 @@ package com.poliedro.jogodonotao.usuario;
  * É usada como base para as classes {@link Aluno} e {@link Professor}.
  */
 public abstract class Usuario {
+    /**
+     * ID do usuário no banco de dados.
+     */
+    protected int id;
+    /**
+     * Nome do usuário.
+     */
+    protected String nome;
+    /**
+     * E-mail do usuário.
+     */
+    protected String email;
+    /**
+     * Hash da senha do usuário.
+     */
+    protected String senha;
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 }
