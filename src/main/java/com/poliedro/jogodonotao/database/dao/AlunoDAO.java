8aeac07adab35bb2e49817e384b8311ba87d861c
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-20 19:24:35 -0300

    Squashed commit: feature/auth/aluno
    commit 05d15ed09a5edcebdad31d3a1034120a579abf95
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 19:24:02 2025 -0300
    
        feat(login-aluno): adição de mensagem de erro se o e-mail ou RA não estiver cadastrado no banco de dados
    
    commit f86d741fddaa9483864c7a2a61c60b59f3855b0f
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 19:19:36 2025 -0300
    
        docs(login-aluno): adição de Javadocs no método `Aluno.iniciarSessao`
    
    commit f416f39dc064cff72209ff013f3b6a9d7c684c04
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 19:16:36 2025 -0300
    
        feat(login-aluno): criação de método que autentica um aluno
    
    commit 5f63262c74ede6d888d0cec8de3c789f5f0c1581
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 19:06:13 2025 -0300
    
        feat(login-aluno): validação do campo de login
    
    commit 47dfa3769dbfb680d9f49242efe03b83da916045
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 18:50:59 2025 -0300
    
        chore(aluno-dao): ajuste no tipo de parâmetro e visibilidade de método
    
    commit 0b4b4d9762be1115b33804b84e0e33bf26b68ad0
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 18:47:22 2025 -0300
    
        chore(aluno-dao): remoção da classe de teste
    
    commit 55bfe56bbe38a8336cf83b5217bea761d5eceda5
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 18:44:17 2025 -0300
    
        feat(aluno-dao): criação de métodos para obter um aluno a partir de seu ID, RA ou e-mail
    
    commit b2c1fc0c075ccdb4a4c056b0ab5cf603e4208c9c
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 13:38:40 2025 -0300
    
        feat(aluno-dao): criação de método para obter um aluno do banco de dados pelo e-mail
    
    commit 03a3edd5c6b59712858332ea2ac99a23a2715d0b
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 13:18:32 2025 -0300
    
        chore(aluno-dao): criação de enum com as colunas da tabela
    
    commit 8c81b5cf92330f2b0ab993497aa71a37717fbbf1
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 12:27:48 2025 -0300
    
        refactor(login-professor): remoção de parâmetro desnecessário
    
    commit 2400cbc8e95bb452fab6d9971ebb5f116d9cc20f
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 12:14:27 2025 -0300
    
        refactor(login-professor): criação de método para fazer a autenticação do professor
    
    commit 8cb608beef6eb801b8d8a94b566b93a82aa5daa8
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Tue May 20 11:45:52 2025 -0300
    
        feat(aluno): criação de atributo para armazenar o aluno logado

--- src/main/java/com/poliedro/jogodonotao/database/dao/AlunoDAO.java
+++ src/main/java/com/poliedro/jogodonotao/database/dao/AlunoDAO.java
@@ -1,4 +1,110 @@
 package com.poliedro.jogodonotao.database.dao;
 
+import com.poliedro.jogodonotao.database.ConexaoDB;
+import com.poliedro.jogodonotao.usuario.Aluno;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Classe DAO para a entidade Aluno.
+ * Responsável por obter e manipular os dados da tabela {@code professor} no banco de dados.
+ *
+ * @author Alexandre Raminelli
+ */
 public class AlunoDAO {
+    /**
+     * Colunas da tabela aluno.
+     */
+    enum AlunoColunas {
+        ID("id_aluno"),
+        ID_TURMA("id_turma"),
+        NOME("nome"),
+        EMAIL("email"),
+        RA("ra"),
+        HASH_SENHA("senha"),
+        PONTUACAO("pontuacao_geral");
+
+        // Construtor
+        private final String coluna;
+
+        AlunoColunas(String coluna) {
+            this.coluna = coluna;
+        }
+
+        // Getter do valor
+        public String get() {
+            return coluna;
+        }
+    }
+
+    /**
+     * Busca aluno no banco de dados a partir de um atributo chave (ID, e-mail ou RA).
+     *
+     * @param coluna O atributo/coluna chave usada na busca. Pode ser ID, e-mail ou RA.
+     * @param chave  O valor que será buscado no banco de dados.
+     * @return Instância do aluno correspondente ou {@code null} se ele não for encontrado.
+     */
+    private static Aluno buscarAluno(AlunoColunas coluna, String chave) {
+        // Query SQL
+        String sql = "SELECT * FROM aluno WHERE " + coluna.get() + " = ?";
+
+        // Executar query
+        try (
+                Connection conexao = ConexaoDB.getConnection();
+                PreparedStatement stmt = conexao.prepareStatement(sql)
+        ) {
+            // Substituir placeholder
+            stmt.setString(1, chave); // e-mail sendo buscado
+            // Executar query e obter resultado
+            ResultSet res = stmt.executeQuery();
+
+            // Extrair tupla correspondente
+            if (res.next()) {
+                return new Aluno(
+                        res.getInt(AlunoColunas.ID.get()),
+                        res.getString(AlunoColunas.NOME.get()),
+                        res.getString(AlunoColunas.EMAIL.get()),
+                        res.getString(AlunoColunas.RA.get()),
+                        res.getString(AlunoColunas.HASH_SENHA.get()),
+                        res.getLong(AlunoColunas.PONTUACAO.get())
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        // Se não for encontrado
+        return null;
+    }
+
+    /**
+     * Procura e retorna uma instância aluno a partir de seu e-mail.
+     *
+     * @param email E-mail acadêmico do aluno.
+     * @return A instância do aluno ou {@code null} se ele não for encontrado.
+     */
+    public static Aluno buscarPorEmail(String email) {
+        return buscarAluno(AlunoColunas.EMAIL, email);
+    }
+
+    /**
+     * Procura e retorna uma instância aluno a partir de seu RA.
+     *
+     * @param ra RA (registro acadêmico) do aluno.
+     * @return A instância do aluno ou {@code null} se ele não for encontrado.
+     */
+    public static Aluno buscarPorRa(String ra) {
+        return buscarAluno(AlunoColunas.RA, ra);
+    }
+
+    /**
+     * Procura e retorna uma instância aluno a partir de seu ID.
+     * @param id ID do aluno no banco de dados.
+     * @return A instância do aluno ou {@code null} se ele não for encontrado.
+     */
+    public static Aluno buscarPorId(int id) {
+        return buscarAluno(AlunoColunas.ID, String.valueOf(id));
+    }
 }
