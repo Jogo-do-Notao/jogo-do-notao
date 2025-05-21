298da0071954f9be950d46cdb71ae1a7ead695c7
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-21 12:38:22 -0300

    Squashed commit: database/sql
    commit 61d5b8357e6e56f8aa2d3e2e0bc03d388a4e2628
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 12:36:49 2025 -0300
    
        fix(sql-ddl):  constraints de validação de email duplicadas e vírgulas nas últimas declarações
    
    commit fe6bd1ac461d51d3c3bd2b39725748fbe4f7f9e5
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 12:28:33 2025 -0300
    
        feat(sql-table-edicao-pergunta): criação da tabela `edicao_pergunta`
    
    commit 0d955155fb726aade44f556a82123ba0da3d6942
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 12:25:53 2025 -0300
    
        chore(sql-ddl): adição de `IF NOT EXISTS` ao criar as tabelas
    
    commit c514648fd8c4f6423439050b706768b0a9ac2a37
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 12:23:45 2025 -0300
    
        feat(sql-table-alternativa): criação da tabela alternativa
    
    commit 11aa19b7d084c189529f17517954caa152430a78
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 12:20:33 2025 -0300
    
        chore(sql-table-pergunta-partida): ajuste na tabela pergunta_partida
    
    commit 2634aec09f0af6377082d79e0ca36784f8b6429a
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 12:18:04 2025 -0300
    
        chore(sql-table-partida): ajuste na tabela partida
    
    commit cc81b706225a15cb25c09dd6ee2d991abd7595ac
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 12:02:29 2025 -0300
    
        chore(sql-table-pergunta): ajuste na tabela pergunta
    
    commit 31992c9b150c5f05594bdd109305734cc98ba23e
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 11:58:16 2025 -0300
    
        fix(sql-table-aluno): remoção de espaçamento no data type
    
    commit dff4cbd25834659eaad04e8f966321f7bab0e4f6
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 11:57:29 2025 -0300
    
        chore(sql-table-pontuacao-materia): ajuste na tabela pontuacao_materia
    
    commit e28718beff66cdcf7df341f6335a448625af6a9c
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 09:21:08 2025 -0300
    
        chore(sql-table-materia): ajuste na tabela materia
    
    commit e204a8af8598fd7cc5fe338fc3ed9c245b21ec9b
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 09:19:48 2025 -0300
    
        chore(sql-table-aluno): ajuste na tabela aluno
    
    commit 6d9d44b0c4e3e9e0ae9bd70d7f9e452846989218
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 09:15:00 2025 -0300
    
        chore(sql-table-turma): ajuste na tabela turma
    
    commit 7245b0d49fd5a5b566df73e4917e3c7bda3edec9
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 09:13:07 2025 -0300
    
        chore(sql-table-professor): ajuste na tabela professor
    
    commit 8fc9381c1c18776b1f79f8ee9bc67a2ff3d479bb
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 09:00:26 2025 -0300
    
        feat(sql-ddl): script para criar/recriar o banco de dados
    
    commit 3b7ca6aec701dc3ed280ef1c024e374072124a7c
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Wed May 21 08:56:21 2025 -0300
    
        refactor(sql-style): formatação do código SQL

--- sql/ddl_create_database.sql
+++ sql/ddl_create_database.sql
@@ -1,50 +1,161 @@
-CREATE TABLE professor
-( id_professor INT PRIMARY KEY,
-nome VARCHAR(300) NOT NULL, email VARCHAR(80) NOT NULL,
-senha CHAR(54) NOT NULL, descricao VARCHAR(100) NULL,
-coordenador TINYINT NOT NULL) ;
-CREATE TABLE turma
-(id_turma INT PRIMARY KEY, professor_responsavel INT,
-FOREIGN KEY (professor_responsavel) 
-REFERENCES professor (id_professor),
-nome VARCHAR(45) NOT NULL, 
-serie INT NOT NULL, descricao VARCHAR(60) NOT NULL) ;
-CREATE TABLE aluno
-(id_aluno INT PRIMARY KEY, id_turma INT,
-FOREIGN KEY (id_turma) 
-REFERENCES turma (id_turma),
-nome VARCHAR (300) NOT NULL, email VARCHAR (80) NOT NULL,
-ra CHAR (8) NOT NULL, senha CHAR (6) NOT NULL,
-pontuacao_geral BIGINT NULL) ;
-CREATE TABLE materia
-(id_materia INT PRIMARY KEY,
-nome VARCHAR (30) NOT NULL) ;
-CREATE TABLE pontuacao_materia
-(id_aluno INT, id_materia INT, FOREIGN KEY (id_aluno) REFERENCES aluno (id_aluno),
-FOREIGN KEY (id_materia) REFERENCES materia (id_materia),
-PRIMARY KEY (id_aluno, id_materia),
-pontuacao BIGINT NOT NULL) ;
-CREATE TABLE pergunta
-(id_pergunta INT PRIMARY KEY, id_materia INT, criador INT,
-FOREIGN KEY (id_materia) REFERENCES materia (id_materia),
-FOREIGN KEY (criador) REFERENCES professor (id_professor),
-titulo VARCHAR(100) NOT NULL,
-dificuldade ENUM('Facil', 'Medio', 'Dificil') NOT NULL,
-dica VARCHAR(100) NOT NULL) ;
-CREATE TABLE partida
-(id_partida INT PRIMARY KEY, id_aluno INT, id_materia INT,
-FOREIGN KEY (id_aluno) REFERENCES aluno (id_aluno),
-FOREIGN KEY (id_materia) REFERENCES materia (id_materia),
-status ENUM('Em andamento', 'Perdida', 'Concluida', 'Abandonada') NOT NULL,
-rodada TINYINT NOT NULL,
-pontuacao_acumulada INT NULL, 
-pontuacao_checkpoint INT NULL,
-ajuda_eliminar TINYINT NULL, ajuda_dica TINYINT NULL,
-ajuda_pular TINYINT NULL) ;
-CREATE TABLE pergunta_partida
-(id_pergunta INT, id_partida INT, FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta),
-FOREIGN KEY (id_partida) REFERENCES partida (id_partida),
-rodada TINYINT,
-PRIMARY KEY (rodada, id_partida));
-
-
+-- Script MySQL DDL para criar o banco de dados e as tabelas
+-- Apagar o banco de dados caso exista (para recriação)
+DROP DATABASE IF EXISTS jogo_do_notao_db;
+-- Criar o banco de dados
+CREATE DATABASE IF NOT EXISTS jogo_do_notao_db;
+USE jogo_do_notao_db;
+-- ---------------------------------------------------------
+-- Criar as tabelas:
+-- Tabela Professor
+CREATE TABLE IF NOT EXISTS professor (
+    -- Colunas:
+    id_professor INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(80) NOT NULL UNIQUE,
+    hash_senha CHAR(64) NOT NULL,
+    descricao VARCHAR(100) NULL,
+    -- se possui permissões de gerenciar outros professores
+    coordenador BOOLEAN NOT NULL DEFAULT FALSE,
+    -- Validações:
+    -- domínio do email
+    CONSTRAINT check_email_professor CHECK (email LIKE '%@sistemapoliedro.com.br')
+);
+-- Tabela Turma
+CREATE TABLE IF NOT EXISTS turma (
+    -- Colunas:
+    id_turma INT PRIMARY KEY AUTO_INCREMENT,
+    professor_responsavel INT NOT NULL,
+    nome VARCHAR(45) NOT NULL,
+    serie INT NOT NULL,
+    descricao VARCHAR(60) NOT NULL,
+    -- FKs:
+    FOREIGN KEY (professor_responsavel) REFERENCES professor (id_professor)
+);
+-- Tabela Aluno
+CREATE TABLE IF NOT EXISTS aluno (
+    -- Colunas:
+    id_aluno INT PRIMARY KEY AUTO_INCREMENT,
+    id_turma INT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(80) NOT NULL UNIQUE,
+    ra CHAR(8) NOT NULL UNIQUE,
+    hash_senha CHAR(64) NOT NULL,
+    pontuacao_geral BIGINT DEFAULT 0,
+    -- FKs:
+    FOREIGN KEY (id_turma) REFERENCES turma (id_turma),
+    -- Validações:
+    CONSTRAINT check_email_aluno CHECK (email LIKE '%@p4ed.com')
+);
+-- Tabela Matéria
+CREATE TABLE IF NOT EXISTS materia (
+    -- Colunas:
+    id_materia INT PRIMARY KEY,
+    nome VARCHAR(30) NOT NULL
+);
+-- Tabela associativa entre Aluno e Matéria
+CREATE TABLE IF NOT EXISTS pontuacao_materia (
+    -- Colunas:
+    id_aluno INT NOT NULL,
+    id_materia INT NOT NULL,
+    pontuacao BIGINT DEFAULT 0,
+    -- PK composta
+    PRIMARY KEY (id_aluno, id_materia),
+    -- FKs:
+    FOREIGN KEY (id_aluno) REFERENCES aluno (id_aluno),
+    FOREIGN KEY (id_materia) REFERENCES materia (id_materia)
+);
+-- Tabela Pergunta
+CREATE TABLE IF NOT EXISTS pergunta (
+    -- Colunas:
+    id_pergunta INT PRIMARY KEY AUTO_INCREMENT,
+    id_materia INT NOT NULL,
+    criador INT NOT NULL,
+    titulo VARCHAR(200) NOT NULL,
+    dificuldade ENUM('Fácil', 'Médio', 'Difícil') NOT NULL,
+    dica VARCHAR(200) NOT NULL,
+    -- FKs:
+    FOREIGN KEY (id_materia) REFERENCES materia (id_materia),
+    FOREIGN KEY (criador) REFERENCES professor (id_professor)
+);
+-- Tabela Alternativa
+CREATE TABLE IF NOT EXISTS alternativa (
+    -- Colunas:
+    id_alternativa INT PRIMARY KEY AUTO_INCREMENT,
+    id_pergunta INT NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    correta BOOLEAN NOT NULL DEFAULT FALSE,
+    -- FKs:
+    FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta)
+);
+-- Tabela Edição-Pergunta
+CREATE TABLE IF NOT EXISTS edicao_pergunta(
+    -- Colunas:
+    id_edicao_pergunta INT PRIMARY KEY AUTO_INCREMENT,
+    id_pergunta INT NOT NULL,
+    id_professor INT NOT NULL,
+    data_hora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    -- FKs:
+    FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta),
+    FOREIGN KEY (id_professor) REFERENCES professor (id_professor)
+);
+-- Tabela Partida
+CREATE TABLE IF NOT EXISTS partida (
+    -- Colunas:
+    id_partida INT PRIMARY KEY AUTO_INCREMENT,
+    id_aluno INT NOT NULL,
+    -- se for null, partida tem perguntas de todas as matérias
+    id_materia INT NULL,
+    -- status da partida
+    status ENUM(
+        'Em Andamento',
+        'Não Concluída',
+        'Ganha',
+        'Perdida',
+        'Abandonada'
+    ) NOT NULL DEFAULT 'Não Concluída',
+    -- rodada atual: entre 1 a 15
+    rodada TINYINT NOT NULL,
+    -- pontuacao da partida
+    pontuacao_acumulada INT DEFAULT 0,
+    pontuacao_checkpoint INT DEFAULT 0,
+    -- uso de ajudas (máx 2)
+    ajuda_eliminar TINYINT NULL,
+    ajuda_dica TINYINT NULL,
+    ajuda_pular TINYINT NULL,
+    -- FKs:
+    FOREIGN KEY (id_aluno) REFERENCES aluno (id_aluno),
+    FOREIGN KEY (id_materia) REFERENCES materia (id_materia),
+    -- Validações:
+    -- rodada esteja entre 1 e 15
+    CONSTRAINT check_rodada CHECK (
+        rodada >= 1
+        AND rodada <= 15
+    ),
+    -- pontuacao seja um valor positivo
+    CONSTRAINT check_pontuacao CHECK (
+        pontuacao_acumulada >= 0
+        AND pontuacao_checkpoint >= 0
+    ),
+    -- ajudas esteja entre 0 e 2
+    CONSTRAINT check_valor_ajuda CHECK (
+        ajuda_eliminar >= 0
+        AND ajuda_eliminar <= 2
+        AND ajuda_dica >= 0
+        AND ajuda_dica <= 2
+        AND ajuda_pular >= 0
+        AND ajuda_pular <= 2
+    )
+);
+-- Tabela associativa entre Pergunta e Partida
+CREATE TABLE IF NOT EXISTS pergunta_partida (
+    -- Colunas:
+    id_pergunta INT NOT NULL,
+    id_partida INT NOT NULL,
+    -- rodada onde a pergunta foi feita na partida
+    rodada TINYINT NOT NULL,
+    -- PK composta
+    PRIMARY KEY (id_partida, rodada),
+    -- FKs:
+    FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta),
+    FOREIGN KEY (id_partida) REFERENCES partida (id_partida)
+);
