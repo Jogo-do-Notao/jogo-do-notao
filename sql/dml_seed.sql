7bbc275adbe9f7620ec271e047cb4715094e1ac2
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-21 15:20:40 -0300

    feat(dml-seed-materia): criação dos dados seed de matérias

--- sql/dml_seed.sql
+++ sql/dml_seed.sql
@@ -1,1 +1,16 @@
 -- Dados para semear o banco de dados com valores pré-definidos, como matérias e conta root
+-- Matérias
+INSERT INTO materia (nome)
+VALUES ('Língua Portuguesa'),
+    ('Matemática'),
+    ('Biologia'),
+    ('Física'),
+    ('Química'),
+    ('História'),
+    ('Geografia'),
+    ('Filosofia'),
+    ('Sociologia'),
+    ('Inglês'),
+    ('Espanhol'),
+    ('Arte'),
+    ('Ciências (Fundamental)')
