c255be85e892a4499bf522d8b23ba6e384bd5744
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-26 13:36:50 -0300

    feat(turma): criação da classe `Turma`

--- src/main/java/com/poliedro/jogodonotao/agrupadores/Turma.java
+++ src/main/java/com/poliedro/jogodonotao/agrupadores/Turma.java
@@ -1,4 +1,61 @@
 package com.poliedro.jogodonotao.agrupadores;
 
+import com.poliedro.jogodonotao.usuario.Professor;
+
+/**
+ * Turma utilizada para agrupar os alunos e facilitar a busca e gerenciamento.
+ */
 public class Turma {
+    // Atributos
+    /**
+     * ID da turma no banco de dados.
+     */
+    private int id;
+    /**
+     * Nome da turma. Usado como identificador.
+     */
+    private String nome;
+    /**
+     * Professor responsável pela turma.
+     */
+    private Professor professor;
+    /**
+     * Série da turma.
+     */
+    private byte serie;
+    /**
+     * Descrição da turma.
+     */
+    private String descricao;
+
+    // Construtor
+    public Turma(int id, String nome, /*Professor professor, */ byte serie, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        //this.professor = professor;
+        this.serie = serie;
+        this.descricao = descricao;
+    }
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public byte getSerie() {
+        return serie;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
