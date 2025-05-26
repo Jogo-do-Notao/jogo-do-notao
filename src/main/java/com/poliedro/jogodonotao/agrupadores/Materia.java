14fc76eebb58118857f14bc623ea325f9e66d057
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-26 20:24:12 -0300

    feat(dao-materia): criação de método para obter uma lista com todas as matéria do DB

--- src/main/java/com/poliedro/jogodonotao/agrupadores/Materia.java
+++ src/main/java/com/poliedro/jogodonotao/agrupadores/Materia.java
@@ -1,4 +1,31 @@
 package com.poliedro.jogodonotao.agrupadores;
 
+/**
+ * Matéria utilizada para classificar as perguntas e pontuações.
+ */
 public class Materia {
+    // Atributos
+    /**
+     * ID da matéria no banco de dados.
+     */
+    private int id;
+    /**
+     * Nome da matéria.
+     */
+    private String nome;
+
+    // Construtor
+    public Materia(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- src/main/java/com/poliedro/jogodonotao/database/dao/MateriaDAO.java
+++ src/main/java/com/poliedro/jogodonotao/database/dao/MateriaDAO.java
@@ -1,5 +1,17 @@
 package com.poliedro.jogodonotao.database.dao;
 
+import com.poliedro.jogodonotao.agrupadores.Materia;
+import com.poliedro.jogodonotao.database.ConexaoDB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Classe DAO para a entidade Matéria. Responsável por obter e manipular os dados da tabela {@code materia} no banco de dados.
+ */
 public class MateriaDAO {
     /**
      * Colunas da tabela {@code materia}.
@@ -19,6 +31,48 @@ public class MateriaDAO {
         // Getter do valor
         public String get() {
             return coluna;
+        }
+    }
+
+    /**
+     * Obtêm uma lista com todas as matérias cadastradas no banco de dados.
+     *
+     * @return Uma lista com todas as matérias cadastradas no banco de dados.
+     */
+    public static ArrayList<Materia> obterMaterias() {
+        // query SQL
+        final String sql = "SELECT * FROM materia";
+        // lista de matérias
+        ArrayList<Materia> materias = new ArrayList<>();
+
+        // Executar query
+        try (
+                Connection conexao = ConexaoDB.getConnection();
+                PreparedStatement stmt = conexao.prepareStatement(sql);
+                ResultSet res = stmt.executeQuery()
+        ) {
+            // Extrair tuplas da tabela
+            while (res.next()) {
+                // extrair atributos
+                int id = res.getInt(MateriaColuna.ID.get());
+                String nome = res.getString(MateriaColuna.NOME.get());
+                // adicionar a lista
+                materias.add(new Materia(id, nome));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e); // tratamento de erro
+        }
+        // retornar lista de matérias
+        return materias;
+    }
+
+    // Teste
+    public static void main(String[] args) {
+        ArrayList<Materia> materias = obterMaterias();
+
+        System.out.println("Matérias:");
+        for (Materia materia : materias) {
+            System.out.println("\t- " + materia.getNome());
         }
     }
 }
