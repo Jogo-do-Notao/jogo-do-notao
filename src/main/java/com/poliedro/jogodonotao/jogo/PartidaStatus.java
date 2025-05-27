2f8dc589976be6140a9360d73d7016ae9aaa9fca
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-27 10:39:01 -0300

    feat(partida): criação do enumeration para status da partida

--- src/main/java/com/poliedro/jogodonotao/jogo/PartidaStatus.java
+++ src/main/java/com/poliedro/jogodonotao/jogo/PartidaStatus.java
@@ -1,4 +1,20 @@
 package com.poliedro.jogodonotao.jogo;
 
 public enum PartidaStatus {
+    // Status possíveis da partida
+    ANDAMENTO("em_andamento"),
+    GANHA("ganha"),
+    PERDIDA("perdida"),
+    ABANDONADA("abandonada");
+
+    // Retornar o status correspondente ao nome
+    private String status;
+
+    PartidaStatus(String status) {
+        this.status = status;
+    }
+
+    public String get() {
+        return status;
+    }
 }
