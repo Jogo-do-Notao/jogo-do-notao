fa01f631a0ea074df662e508a9ac8a6037853ee2
viniciusct0626 <vct.vctavarez@gmail.com>
2025-05-28 09:16:55 -0300

    Adicionando ControleCadastarProfessor

---
+++ src/main/java/com/poliedro/jogodonotao/controller/ControleCadastarProfessor.java
@@ -0,0 +1,34 @@
+package com.poliedro.jogodonotao.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ControleCadastarProfessor {
+
+    @FXML
+    private TextField campoConfirmarSenha;
+
+    @FXML
+    private TextField campoDescrição;
+
+    @FXML
+    private TextField campoEmail;
+
+    @FXML
+    private TextField campoNome;
+
+    @FXML
+    private TextField campoSenha;
+
+    @FXML
+    void adicionarProfessor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarParaPainel(ActionEvent event) {
+
+    }
+
+}
