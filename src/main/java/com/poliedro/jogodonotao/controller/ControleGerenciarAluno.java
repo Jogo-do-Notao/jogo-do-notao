c719f97f64174cb0f34a5db8301bb1592cdfdd17
viniciusct0626 <vct.vctavarez@gmail.com>
2025-05-28 08:55:25 -0300

    Arrumando ControleAdicionarAluno

--- src/main/java/com/poliedro/jogodonotao/controller/ControleGerenciarAluno.java
+++ src/main/java/com/poliedro/jogodonotao/controller/ControleGerenciarAluno.java
@@ -1,8 +1,11 @@
 package com.poliedro.jogodonotao.controller;
 
+import com.poliedro.jogodonotao.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class ControleGerenciarAluno {
     //falta adicionar id e acoes para a tabela
@@ -10,12 +13,12 @@ public class ControleGerenciarAluno {
     private TextField campoPesquisarAluno;
 
     @FXML
-    void adicionarAluno(ActionEvent event) {
+    void adicionarAluno(ActionEvent event) throws IOException {
         App.changeScene("area-adm/gerenciar-alunos/tela-gerenciar-alunos", "Tela Adicionar Aluno");
     }
 
     @FXML
-    void voltarParaPainel(ActionEvent event) {
+    void voltarParaPainel(ActionEvent event) throws IOException {
         App.changeScene("area-adm/painel-administrador", "Painel Admnistrador");
     }
 
