69d54b499d670af2e2a56d2080609b5ce4371d1b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-05-29 12:28:16 -0300

    feat(tela-partida): criação da classe controladora

--- src/main/java/com/poliedro/jogodonotao/controller/areaAluno/partidas/ControleTelaPartida.java
+++ src/main/java/com/poliedro/jogodonotao/controller/areaAluno/partidas/ControleTelaPartida.java
@@ -1,7 +1,85 @@
 package com.poliedro.jogodonotao.controller.areaAluno.partidas;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
+
 /**
- * Controle da Tela de Partida.
+ * Controle da tela de partida do jogo.
+ * Responsável por gerenciar os eventos e a interação do usuário durante uma partida.
  */
 public class ControleTelaPartida {
+
+    @FXML
+    private ToggleGroup alternativaGroup;
+
+    @FXML
+    private ProgressBar barraProgresso;
+
+    @FXML
+    private Text textEnunciado;
+
+    @FXML
+    private Text textMateria;
+
+    @FXML
+    private Text textPontuacaoAcumulada;
+
+    @FXML
+    private Text textPontuacaoCheckpoint;
+
+    @FXML
+    private Text textProgresso;
+
+    @FXML
+    private Text usoAjudaCartas;
+
+    @FXML
+    private Text usoAjudaDica;
+
+    @FXML
+    private Text usoAjudaPular;
+
+    @FXML
+    void alternativaSelecionada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void desistirDaPartida(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exibirInstrucoes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sairDaPartida(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usarAjudaCartas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usarAjudaDica(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usarAjudaPular(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verificarResposta(ActionEvent event) {
+
+    }
+
 }
