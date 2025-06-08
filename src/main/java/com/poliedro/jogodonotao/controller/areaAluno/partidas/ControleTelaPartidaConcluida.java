b9aa0be5494fbf71ba148c168096f0bb954ee258
Pgs-0 <25.01699-9@maua.br>
2025-06-08 14:38:44 -0300

    Criação Controller Partida Concluida

---
+++ src/main/java/com/poliedro/jogodonotao/controller/areaAluno/partidas/ControleTelaPartidaConcluida.java
@@ -0,0 +1,12 @@
+package com.poliedro.jogodonotao.controller.areaAluno.partidas;
+
+import javafx.event.ActionEvent;
+
+public class ControleTelaPartidaConcluida {
+
+    @FXML
+    void voltarMenuAluno(ActionEvent event) {
+
+    }
+
+}
