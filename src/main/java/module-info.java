91121abc43a4e8ddb5b14909b53dd4de59d5c655
Pgs-0 <25.01699-9@maua.br>
2025-06-09 13:09:43 -0300

    Correção de Bugs no module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,10 +9,11 @@ module com.poliedro.jogodonotao {
     opens com.poliedro.jogodonotao to javafx.fxml, javafx.base;
     opens com.poliedro.jogodonotao.usuario to javafx.base, javafx.fxml;
     opens com.poliedro.jogodonotao.controller to javafx.fxml;
-    
+    opens com.poliedro.jogodonotao.controller.areaAluno.partidas to javafx.fxml;
     // Exporta os pacotes necessários
     exports com.poliedro.jogodonotao;
     exports com.poliedro.jogodonotao.usuario;
     exports com.poliedro.jogodonotao.controller;
     exports com.poliedro.jogodonotao.agrupadores;
+    exports com.poliedro.jogodonotao.controller.areaAluno.partidas to javafx.fxml;
 }
